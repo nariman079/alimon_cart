4c081e6f6a873f0d12363620c040b8148dd76baf
nariman079 <nariman079i@mail.ru>
2025-01-13 14:18:12 +0300

    fix pipelines

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from redis import Redis
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
-redis = Redis()
+
 
 current_directory: Path = Path.cwd()
 
@@ -16,7 +16,11 @@ if sys.platform == "win32":
 
 PRODUCTION_MODE = getenv('PRODUCTION_MODE', '0') == '1'
 
+REDIS_HOST = getenv("REDIS_HOST", 'localhost')
+REDIS_PORT = getenv("REDIS_PORT", 6379)
+
 if PRODUCTION_MODE:
+    
     POSTGRES_USER = getenv("POSTGRES_USER")
     POSTGRES_PASSWORD = getenv("POSTGRES_PASSWORD")
     POSTGRES_DB = getenv("POSTGRES_DB")
@@ -24,7 +28,7 @@ if PRODUCTION_MODE:
     POSTGRES_PORT = getenv("POSTGRES_PORT")
 
     DB_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-
+    
 else:
     DB_URL = "postgresql+asyncpg://test:test@localhost:5431/test"
 
@@ -37,6 +41,7 @@ engine = create_async_engine(
 
 async_session = async_sessionmaker(bind=engine, expire_on_commit=False)
 
+redis = Redis(host=REDIS_HOST, port=REDIS_PORT)
 
 ALGORITHM = getenv('ALGORITHM', 'HS256')
 SECRET = getenv('SECRET_KEY', 'secret')
