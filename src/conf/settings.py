f4c875546cae18301a5bb17d8d74d6c2fee5c1ca
nariman079 <nariman079i@mail.ru>
2025-01-14 14:01:17 +0300

    fix errors in postgresql

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -3,9 +3,11 @@ import sys
 from os import getenv
 from pathlib import Path
 
+from dotenv import load_dotenv
 from redis import Redis
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
+load_dotenv()
 
 
 current_directory: Path = Path.cwd()
@@ -30,6 +32,12 @@ if PRODUCTION_MODE:
 else:
     DB_URL = "postgresql+asyncpg://test:test@localhost:5431/test"
 
+print(f"POSTGRES_USER: {POSTGRES_USER}")
+print(f"POSTGRES_PASSWORD: {POSTGRES_PASSWORD}")
+print(f"POSTGRES_DB: {POSTGRES_DB}")
+print(f"POSTGRES_HOST: {POSTGRES_HOST}")
+print(f"POSTGRES_PORT: {POSTGRES_PORT}")
+print(DB_URL)
 
 engine = create_async_engine(
     DB_URL,
